8126258cbadb265d2e15cc9c80fcff7d67631592
Javier L <javicoll97haz@hotmail.com>
2021-05-19 10:59:21 -0500

    Proyecto Final
    Versión inicial de proyecto

---
+++ WebApplication5/WebApplication5/Models/Clientes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Prestamos.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "El campo {0} esta vacio.")]
+        [StringLength(60)]
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} esta vacio.")]
+        [StringLength(60)]
+        public String Apellidos { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} esta vacio.")]
+        [StringLength(60)]
+        public String Direccion { get; set; }
+
+        public virtual ICollection<Prestamo> Prestamos { get; set; }
+    }
+}
